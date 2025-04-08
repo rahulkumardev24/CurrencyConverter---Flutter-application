12bcc996536484ef03112cd4864a79884c6441fe
rahul <rkrahulroy151617@gmail.com>
2025-04-08 16:06:50 +0530

    hit api successfully

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:currency_converter/model/currency_model.dart';
 import 'package:currency_converter/service/currency_service.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -13,21 +14,42 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   CurrencyService currencyService = CurrencyService();
+  Future<CurrencyModel?>? _currency;
+
 
   @override
   void initState() {
     super.initState();
+    _currency = currencyService.fetchCurrency();
+
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(onPressed: ()  {
-         currencyService.fetchCurrency();
-        }, child: Text("Hit API")),
+      body:FutureBuilder<CurrencyModel?>(
+        future: _currency,
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return CircularProgressIndicator();
+          }else if(snapshot.hasError){
+            return Center(child: Text("Error Found : ${snapshot.hasError}"),);
+          }else if(snapshot.hasData){
+            final currencyData = snapshot.data;
+            final currencyRate = currencyData!.rates;
+            return Center(child: Container(child: Column(
+              children: [
+                Text(currencyRate!.iNR ?? "No Found"),
+                Text(currencyRate!.uSD ?? "No Found"),
+                Text(currencyRate!.aIXBT ?? "No Found"),
+                Text(currencyRate!.aGLD ?? "No Found"),
+              ],
+            ),));
+          }else {
+            return Center(child: Text("No data Found"),);
+          }
+        },
       ),
     );
   }
-
-
 }
